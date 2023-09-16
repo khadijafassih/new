5e2181656c1ae28f58f35247f710bee5811a45e5
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2023-09-17 00:00:55 +0500

    Add files via upload

---
+++ pikachu.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main() 
+{
+system("color 4A");
+cout << "             ';-..         .---'..          \n";                          
+cout << "               '. '!_...._/'.-''            \n";                           
+cout << "                 !           /      ,       \n";                   
+cout << "                 ( 0     0   )!   .' '-._   \n";                       
+cout << "                (     .       )! /  .. _.'  \n";                       
+cout << "                 {   -'-     ;  '. <        \n";                   
+cout << "                 ;.__      .;|   .> |       \n";                       
+cout << "                 / '    / '  |.-'._'        \n";                
+cout << "                (O/    (0/   ).<'           \n";                  
+cout << "                  !    '     ;-'            \n";             
+cout << "                   >   !    /               \n";            
+cout << "                  (_,-''>.'                 \n";                    
+cout << "                    (_,'                    \n"; 
+system("color E0");                                                                            
+}
