c4c0343f5b18d60d5ce0638aa2e5306297c33565
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2023-09-16 22:16:45 +0500

    Add files via upload

---
+++ cow.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 17");
+cout<< "_________________________\n";
+cout<< "o   ^___^\n";
+cout<< " o  (O O)\\___________\n";
+cout<< "    (-W-)\\           )~~~~~\n";
+cout<< "     WWW ||---------W-| \n";
+cout<< "         ||          || \n";
+}
